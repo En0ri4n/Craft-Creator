0f6e8b908502cbb27e91feea21ac5a7c2001958f
En0ri4n <enorian.rajo@gmail.com>
2023-01-08 16:35:31 +0100

    Data packet implementation

---
+++ src/main/java/fr/eno/craftcreator/tileentity/vanilla/InventoryDataContainerTileEntity.java
@@ -0,0 +1,17 @@
+package fr.eno.craftcreator.tileentity.vanilla;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+public abstract class InventoryDataContainerTileEntity extends InventoryContainerTileEntity
+{
+    public InventoryDataContainerTileEntity(BlockEntityType<?> pType, BlockPos pWorldPosition, BlockState pBlockState, int inventorySize)
+    {
+        super(pType, pWorldPosition, pBlockState, inventorySize);
+    }
+
+    public abstract void setData(String dataName, Object data);
+
+    public abstract Object getData(String dataName);
+}
